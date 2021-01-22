52749ee208eadf362e31a5efe1df0ac6bca3dd0a
Bleak Chandler <bleakchandler@gmail.com>
2021-01-22 19:29:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+newinfo = "C", "Assembly"
+     
+  programmer_hash[:dennis_ritchie][:languages] = newinfo
+   programmer_hash
 end
